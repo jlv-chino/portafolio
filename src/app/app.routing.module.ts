b32365ee6cc7f6d9a15aa10dccd258783a52532a
José Luis Vilches <jose.luis.comvilches@gmail.com>
2021-11-17 03:06:57 -0300

    Enlazamos las rutas con los menús, habiltamos el uso de hash

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,15 +5,15 @@ import { NosotrosComponent } from "./pages/nosotros/nosotros.component";
 import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
 
 const app_routes: Routes = [
-    { path: '', component: PortafolioComponent },
+    { path: 'inicio', component: PortafolioComponent },
     { path: 'nosotros', component: NosotrosComponent},
     { path: 'item', component: ItemComponent},
-    { path: '**', pathMatch: 'full' ,redirectTo: ''}
+    { path: '**', pathMatch: 'full' ,redirectTo: 'inicio'}
 ]
 
 @NgModule({
     imports: [
-        RouterModule.forRoot( app_routes )
+        RouterModule.forRoot( app_routes, {useHash: true} )
     ],
     exports: [
         RouterModule
